b7500d07b52ad89ee3a6712d12fcb4bed7740035
Almeida <almeidasmatheus909@gmail.com>
2022-03-21 09:45:23 -0300

    Tarefa 4

---
+++ roomScript.js
@@ -0,0 +1,8 @@
+function lightSwitch(){
+				if(document.getElementById("1").className==="light lightOn"){
+								document.getElementById("1").setAttribute("class", "light lightOff")
+				}
+				else{
+								document.getElementById("1").setAttribute("class", "light lightOn")
+				}
+}
